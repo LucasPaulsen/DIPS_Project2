61938be83d4fb42ff571ee17671e8dce5a85d4b1
Lucas <Lucas_paulsen@yahoo.dk>
2022-04-25 15:48:38 +0200

    Added sub-topics and kitchen sensor

--- 1101_phidget_CSharp_VisualStudio/SubscriberService/Program.cs
+++ 1101_phidget_CSharp_VisualStudio/SubscriberService/Program.cs
@@ -11,16 +11,18 @@ namespace SubscriberService
     {
         static MqttClient client;
         static readonly string PILL_TAG = "0107ee832e";
-        static readonly string PIR_ID = "DIPS8/0x842e14fffe065bbc";
+        static readonly string PIR_BEDROOM = "DIPS8/0x842e14fffe065bbc";
+        static readonly string PIR_KITCHEN = "DIPS8/0x842e14fffe47a4f3";
+        static readonly string BEDROOM_SCREEN = "DIPS8/BEDROOM_SCREEN";
+        static readonly string KITCHEN_SCREEN = "DIPS8/KITCHEN_SCREEN";
         static readonly TimeSpan startMorning = new TimeSpan(12, 0, 0); // Modified while debugging
         static readonly TimeSpan endMorning = new TimeSpan(15, 0, 0); // Modified while debugging
-        private static readonly TimeSpan pillPlaceThreshold = new TimeSpan(0, 0, 20); // time to put pillbox back to its place
         static readonly int MIN_PILL_DURATION = 5; // Spend at least 5 seconds taking pills 
         static bool pillsTaken = false;
         static bool isAwake = false;
-        static bool pillboxOff;
         static long pillsOffTime = -1;
         private static Timer timer;
+        static DateTimeOffset breakfastTime;
 
         private static void breakfastTimer(TimeSpan breakfastTime) { 
             DateTime current = DateTime.Now;
@@ -39,26 +41,23 @@ namespace SubscriberService
         {
             TimeSpan currentTime = new DateTimeOffset(DateTime.Now).TimeOfDay;
             Console.WriteLine("Time is now "+ currentTime +". You can eat breakfast");
-        }
-
-        private static void SetPillboxReminder(long pillsOffTime)
-        {
-            var pillsOffDateTime = DateTime.FromBinary(pillsOffTime);
-            do
-            {
-                var currentTime = DateTime.Now;
-                var timeDifference = currentTime.Subtract(pillsOffDateTime);
-                if (timeDifference <= pillPlaceThreshold) continue;
-                Console.WriteLine("Remember to set your pill box back to where it belongs.");
-                return;
-            } while (pillboxOff);
+            client.Publish(BEDROOM_SCREEN, Encoding.ASCII.GetBytes("BREAKFAST_TIME: " + currentTime));
         }
 
 
         private static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
         {
             string received = Encoding.UTF8.GetString(e.Message);
-            if(e.Topic == PIR_ID)
+            if(e.Topic == PIR_KITCHEN)
+            {
+                TimeSpan currentTime = new DateTimeOffset(DateTime.Now).TimeOfDay;
+                if (pillsTaken && currentTime < breakfastTime.TimeOfDay)
+                {
+                    Console.WriteLine("Remember to wait before eating");
+                    client.Publish(KITCHEN_SCREEN, Encoding.ASCII.GetBytes("WAIT: " + breakfastTime.TimeOfDay));
+                }
+            }
+            if(e.Topic == PIR_BEDROOM)
             {
                 TimeSpan currentTime = new DateTimeOffset(DateTime.Now).TimeOfDay;
                 if (currentTime > startMorning && currentTime < endMorning && !isAwake)
@@ -69,42 +68,49 @@ namespace SubscriberService
                     if (occupied.ToString() == "True")
                     {
                         Console.WriteLine("Good morning! Remember your pill");
+                        client.Publish(BEDROOM_SCREEN, Encoding.ASCII.GetBytes("GOOD_MORNING"));
                         isAwake = true;
                     }
                 }
             }
             //Console.WriteLine("Received from broker: " + received);
             String[] receivedProps = received.Split(':');
-            if ((receivedProps[0] == "Tag on" || receivedProps[0] == "Tag off") && !pillsTaken && isAwake)
+            if ((receivedProps[0] == "Tag on" || receivedProps[0] == "Tag off") && isAwake)
             {
                 String tag = receivedProps[1].Trim(' ');
                 //Console.WriteLine("Tag: " + tag);
                 if (receivedProps[0] == "Tag off" && tag == PILL_TAG)
                 {
                     Console.WriteLine("Pills off");
-                    pillsOffTime = new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds();
-                    pillboxOff = true;
-                    SetPillboxReminder(pillsOffTime);
+                    if (pillsTaken)
+                    {
+                        Console.WriteLine("DON'T YOU DARE TAKE MORE PILLS!");
+                        client.Publish(KITCHEN_SCREEN, Encoding.ASCII.GetBytes("PILLS_TAKEN_AGAIN"));
+                        client.Publish(BEDROOM_SCREEN, Encoding.ASCII.GetBytes("PILLS_TAKEN_AGAIN"));
+                    }
+                    else
+                    {
+                        pillsOffTime = new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds();
+                    }
                 }
-                if (receivedProps[0] == "Tag on" && tag == PILL_TAG)
+                if (receivedProps[0] == "Tag on" && tag == PILL_TAG && !pillsTaken)
                 {
-                    if(pillsOffTime != -1)
+                    if (pillsOffTime != -1)
                     {
                         var endTime = new DateTimeOffset(DateTime.Now);
                         var timeSpent = endTime.ToUnixTimeSeconds() - pillsOffTime;
-                        pillboxOff = false;
-                        if(timeSpent >= MIN_PILL_DURATION) // Spend at least 5 seconds taking pills
+                        if (timeSpent >= MIN_PILL_DURATION) // Spend at least 5 seconds taking pills
                         {
                             pillsTaken = true;
                             //var breakfastTime = endTime.AddHours(1);
-                            var breakfastTime = endTime.AddSeconds(10);
+                            breakfastTime = endTime.AddSeconds(10);
+                            client.Publish(BEDROOM_SCREEN, Encoding.ASCII.GetBytes("PILLS_TAKEN: " + endTime.TimeOfDay));
                             Console.WriteLine("Good job! You took the pills at: " + endTime.TimeOfDay);
                             Console.WriteLine("Remember to wait an hour before eating! \nI will remind you to eat breakfast at: " + breakfastTime.TimeOfDay);
                             breakfastTimer(breakfastTime.TimeOfDay);
                         }
                     }
                 }
-
             }
         }
 
@@ -119,9 +125,12 @@ namespace SubscriberService
             if (client.IsConnected)
             {
                 client.Subscribe(new string[] { "DIPS8" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-                client.Subscribe(new string[] { PIR_ID }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+                client.Subscribe(new string[] { "DIPS8/pills" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+                client.Subscribe(new string[] { PIR_BEDROOM }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+                client.Subscribe(new string[] { PIR_KITCHEN }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
             }
             //Console.WriteLine("Hello World!");
+            Console.WriteLine("BreakfastTime: " + breakfastTime.TimeOfDay);
         }
     }
 }
